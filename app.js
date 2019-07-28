a8c69ba26d302a59f9a896263828258eb99aecf1
Kong Chae Won <chaewonkong@gmail.com>
2019-07-28 20:48:42 +0900

    fix: comment dotenv related

--- app.js
+++ app.js
@@ -17,25 +17,25 @@ if (process.env.NODE_ENV === "production") {
 } else {
   app.use(morgan("dev"));
 }
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-const sessionOption = {
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false
-  }
-};
-if (process.env.NODE_ENV === "production") {
-  sessionOption.proxy = true;
-  sessionOption.cookie.secure = true;
-}
-app.use(session(sessionOption));
-app.use(flash());
+// app.use(cookieParser(process.env.COOKIE_SECRET));
+// const sessionOption = {
+//   resave: false,
+//   saveUninitialized: false,
+//   secret: process.env.COOKIE_SECRET,
+//   cookie: {
+//     httpOnly: true,
+//     secure: false
+//   }
+// };
+// if (process.env.NODE_ENV === "production") {
+//   sessionOption.proxy = true;
+//   sessionOption.cookie.secure = true;
+// }
+// app.use(session(sessionOption));
+// app.use(flash());
 
 app.use("/", indexRouter);
 
@@ -45,12 +45,12 @@ app.use((req, res, next) => {
   next(err);
 });
 
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.status(err.status || 500);
-  // res.send(500);
-});
+// app.use((err, req, res, next) => {
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+//   res.status(err.status || 500);
+//   // res.send(500);
+// });
 
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기중");
